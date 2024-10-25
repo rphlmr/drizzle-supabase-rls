34344016bdce4c0aa873b2802013f1e2add13ab2
rphlmr <raphael@uaerom.com>
2024-10-25 16:06:47 +0200

    init

---
+++ drizzle.config.ts
@@ -0,0 +1,17 @@
+/**
+ * This is the configuration for the server-side database.
+ */
+
+import { defineConfig } from "drizzle-kit";
+
+const base = "./database";
+
+export default defineConfig({
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+  schema: `${base}/schema.ts`,
+  out: `${base}/migrations`,
+  verbose: false,
+});
